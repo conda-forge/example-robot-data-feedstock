19493bc1818c987ce34e7f426c81e23d1cd9109b
Justin Carpentier <justin.carpentier@inria.fr>
2020-08-14 09:41:48 +0200

    recipe: fix SITELIB

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,13 +2,14 @@ mkdir build
 if errorlevel 1 exit 1
 
 cd build
+echo %SP_DIR%
 
 ::Configure
 cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DPYTHON_EXECUTABLE=%PYTHON% ^
-    -DPYTHON_SITELIB=%SP_DIR% ^
+    -DPYTHON_SITELIB=%PREFIX% ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
