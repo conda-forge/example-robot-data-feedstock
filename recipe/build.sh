53b6a24ad775e92e0335731fbeda0a158ac8bd93
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-22 14:33:16 +0200

    recipe: fix pinocchio dir for cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,14 @@
 mkdir build
 cd build
 
+export BUILD_SP_DIR=$( $PYTHON -c "import site; print(site.getsitepackages())[0]")
+export TARGET_SP_DIR=$SP_DIR
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo "Copying files from $BUILD_SP_DIR to $TARGET_SP_DIR"
+  cp -r $BUILD_SP_DIR/pinocchio $TARGET_SP_DIR/pinocchio
+fi
+
 cmake .. \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
