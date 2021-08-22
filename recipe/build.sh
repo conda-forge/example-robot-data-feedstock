0aa3a3f9e88cb22967f2643cd876d3887fa04d59
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-22 16:20:39 +0200

    recipe: fix search path

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,8 @@
 mkdir build
 cd build
 
-export BUILD_SP_DIR=$( $PYTHON -c "import site; print(site.getsitepackages())[0]")
+export BUILD_SP_DIR=$( $PYTHON -c "import site; print(site.getsitepackages()[0])")
+export BUILD_SP_DIR=$( $PYTHON -c "import pinocchio; print (pinocchio.__file__.split('/pinocchio/__init__.py')[0])")
 export TARGET_SP_DIR=$SP_DIR
 
 if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
